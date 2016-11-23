c6224e9c9a5d91be47f56877208b6dcf0146fb9b
Leon Chen <leonchen@live.nl>
2016-11-23 14:22:00 +0100

    begin maken

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php ?>
+
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+
+</body>
+</html>
