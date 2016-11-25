583c1b4cd240083662065a853bc2f4432a023960
Leon Chen <leonchen@live.nl>
2016-11-25 16:10:37 +0100

    wat aanpassen qua design

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
         <img class="EenmaalAndermaal" src="images/logo.png" alt="Logo EenmaalAndermaal">
     </p>
 </header>
+
 <div class="container">
     <h1>Muziek, Film & Games</h1>
 
@@ -32,57 +33,52 @@
         <a class="list-group-item" href="#">Sport & Vrije tijd</a>
     </div>
     <div class="col-sm-9">
-        <div class="veilingitem row">
+        <div class="veilingitem">
             <a href="#">
-                <div class="col-sm-10">
-                    <img src="logo.jpg" alt="veilingsitem">
-                    <h4>Veilingsitem 1</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-                        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-                        pellentesque eu,   asd sa as asd  iu eiqwewquiewuqiyqweuqy uiqwyeiwyewuey wuqey wquey</p>
-                </div>
-                <div class="col-sm-2">
-                    <p class="prijsdatum">€14.99<br>2:59:00</p>
-                </div>
+                <img src="logo.jpg" alt="veilingsitem">
+                <h4>Veilingsitem 1</h4>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p class="prijs">€14.99</p> <p class="tijd">2:59:00 <button class="btnvoorwerp">Meer informatie</button> </p>
             </a>
         </div>
-        <div class="veilingitem row">
+        <div class="veilingitem">
             <a href="#">
-                <div class="col-sm-10">
-                    <img src="logo.jpg" alt="veilingsitem">
-                    <h4>Veilingsitem 1</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-                        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-                        pellentesque eu,   asd sa as asd  iu eiqwewquiewuqiyqweuqy uiqwyeiwyewuey wuqey wquey</p>
-                </div>
-                <div class="col-sm-2">
-                    <p class="prijsdatum">€14.99<br>2:59:00</p>
-                </div>
+                <img src="logo.jpg" alt="veilingsitem">
+                <h4>Veilingsitem 1</h4>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p class="prijs">€14.99</p> <p class="tijd">2:59:00 <button class="btnvoorwerp">Meer informatie</button> </p>
             </a>
         </div>
-        <div class="veilingitem row">
+        <div class="veilingitem">
             <a href="#">
-                <div class="col-sm-10">
-                    <img src="logo.jpg" alt="veilingsitem">
-                    <h4>Veilingsitem 1</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-                        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-                        pellentesque eu,   asd sa as asd  adas daasd asdassa ad asd as as a as asasdas sd asd sa asd sad ad asdsad iu eiqwewquiewuqiyqweuqy uiqwyeiwyewuey wuqey wquey</p>
-                </div>
-                <div class="col-sm-2">
-                    <p class="prijsdatum">€14.99<br>2:59:00</p>
-                </div>
+                <img src="logo.jpg" alt="veilingsitem">
+                <h4>Veilingsitem 1</h4>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p class="prijs">€14.99</p> <p class="tijd">2:59:00 <button class="btnvoorwerp">Meer informatie</button> </p>
+            </a>
+        </div>
+        <div class="veilingitem">
+            <a href="#">
+                <img src="logo.jpg" alt="veilingsitem">
+                <h4>Veilingsitem 1</h4>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+                    magnis dis parturient montes, nascetur ridiculus mus. </p>
+                <p class="prijs">€14.99</p> <p class="tijd">2:59:00 <button class="btnvoorwerp">Meer informatie</button> </p>
             </a>
         </div>
     </div>
-</div>
 
-<footer>
-    <p> Copyright © EenmaalAndermaal 2016 </p>
-    <p> Alle rechten voorbehouden </p>
-</footer>
+    <footer>
+        <p> Copyright © EenmaalAndermaal 2016 </p>
+        <p> Alle rechten voorbehouden </p>
+    </footer>
+</div>
 </body>
 </html>
