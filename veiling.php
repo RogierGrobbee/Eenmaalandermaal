ce5218269f5fde49b604bf3680bb5b2b1c0d9f8b
Leon Chen <leonchen@live.nl>
2016-11-29 16:08:49 +0100

    detailpagina, niet echt mooi maar ik krijg hem anders niet goed responsive

---
+++ veiling.php
@@ -0,0 +1,36 @@
+<?php include_once('partial files\header.php')?>
+    <h1>Leuke voorwerp</h1>
+<?php include_once('partial files\sidebar.php') ?>
+    <div class="col-sm-9 veiling">
+        <div class="row">
+            <img src="logo.jpg" alt=" geveilde voorwerp">
+
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-7 details">
+                <div class="prijstijd">
+                    <div class="veilingprijs">€24.99</div>
+                    <div class="veilingtijd">0:10:39</div>
+                </div>
+
+                <p>gerard92 (Almelo, Nederland)</p>
+
+                <h4>Beschrijving</h4>
+                <p>Ik heb een mooie boek en ik wil deze graag verk ik wil deze graag verkopen via deze geweldige site genaamd
+                    EenmaalAndermaal. Het is wel een ik wil deze graag verkopen via deze geweldige site genaamd EenmaalAndermaal. Het
+                    is wel ee qdw wqd  ik wil deze graag verkopen via deze geweldige site genaamd EenmaalAndermaal. Het is wel een ik wil
+                    deze graag verkopen via deze geweldige site genaamd Eenma dw alAndermaal. Het is wel een ik wil deze graag verkopen
+                    via deze geweldige site genaamd EenmaalAndermaal. Het is wel dqwdqwdw qwd qw wqd qeen ik wil deze grdqwedwqdqwdwqaag
+                    verkopen via deze geweldige site genaamd EenmaalAndermaal. Het is wel een dwdwopen via deze geweldige site genaamd
+                    EenmaalAndermaal. Het is wel een luisterboek dus hij kost wel iets duurder, ik hou veel van boeken maar deze boek is zo
+                    leuk dat ik hem wil verkopen adlk admlasdmslkmsakldamdl askldm ads masld malkad d ansd kasndk adsnjk n ik weet niet waar
+                    ik over moet schrijven misschien had ik beter lorem ipsum kunnen typen.</p>
+            </div> <!--maximale aantal karakters-->
+
+            <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4 betaalinformatie">
+                <h4>Betalingswijze- en instructie</h4>
+                <p>Contant, ik hou van contant geld dus geef me geld </p> <!--Dus betalingswijze eerst, dan een komma
+                                                                                en dan de betalingsinstructie-->
+                <h4>Verzendkosten- en instructie</h4>
+                <p>3.00 verzendkosten, breekbaar</p> <!--eerst getal, daarna "verzendkosten," daarna de verzendinstructie-->
+            </div>
+        </div>
+<?php include_once('partial files\footer.php')?>
