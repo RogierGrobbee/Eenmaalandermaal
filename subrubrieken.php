474204dc48a1c42c1ec78c07bd3ac601e27088a2
Rogier Grobbee <r.grobbee@gmail.com>
2016-11-30 10:23:55 +0100

    Alle php code toegevoegd. Sidebar, rubrieken, subrubrieken en veilingen uit de db

---
+++ subrubrieken.php
@@ -0,0 +1,23 @@
+<?php
+function loadSubrubrieken($rubriekArray, $selectedRubriek)
+{
+    $hasSubrubriek = false;
+    foreach ($rubriekArray as $k => $rubriek) {
+        if ($rubriek->superrubriek == $selectedRubriek->rubrieknummer && $rubriek->superrubriek != null) {
+            $hasSubrubriek = true;
+        }
+    }
+    if ($hasSubrubriek) {
+        echo '<div class="row subrubrieken">
+            <h2>Subrubrieken</h2>';
+        foreach ($rubriekArray as $k => $rubriek) {
+            if ($rubriek->superrubriek == $selectedRubriek->rubrieknummer && $rubriek->superrubriek != null) {
+                echo '<div class="col-xs-6 col-md-4 col-lg-3">
+                    <a href=\index.php?rubriek=' . $rubriek->rubrieknummer . '#>' . $rubriek->rubrieknaam . '</a>
+                </div>';
+            }
+        }
+        echo '</div>';
+    }
+}
+?>
---
+++ veilingsTabs.php
@@ -0,0 +1,40 @@
+<?php
+//function loadSubrubrieken ($rubriekId)
+//{
+//    echo 'Hallo hier zijn de subrubrieken';
+//    $db = new PDO ("sqlsrv:Server=LAPTOP-AOSH53E4\SQLEXPRESS;Database=eenmaalandermaal;ConnectionPooling=0", "sa", "Kanarie//////////");
+//
+//    $query = $db->query('SELECT rubrieknummer FROM rubriek WHERE superrubriek = 1');
+//
+//    while($rubriek = $query->fetch(PDO::FETCH_OBJ)) {
+//        echo $rubriek->rubrieknummer. '<br>';
+//    }
+//}
+function loadVeilingItems($rubriekId)
+{
+    if (is_numeric($rubriekId)) {
+        $db = new PDO ("sqlsrv:Server=LAPTOP-AOSH53E4\SQLEXPRESS;Database=eenmaalandermaal;ConnectionPooling=0", "sa", "Kanarie//////////");
+        $query = $db->query("select * from voorwerp where voorwerpnummer in(
+	                          select voorwerpnummer from voorwerpinrubriek where rubriekoplaagsteniveau in
+	                          (
+		                        select rubrieknummer from rubriek where superrubriek='".$rubriekId."' or rubrieknummer = '".$rubriekId."'
+	                          )
+                            )");
+        while ($voorwerp = $query->fetch(PDO::FETCH_OBJ)) {
+
+          echo'  <div class="veilingitem">
+                    <a href="#">
+                        <img src="logo.jpg" alt="veilingsfoto">
+                        <h4>'. $voorwerp->titel .'</h4>
+                        <p>' . $voorwerp->beschrijving . '</p>
+                        <p class="prijs">€' . $voorwerp->startprijs . '</p> <div class="veilingInfo"><span class="tijd">'.$voorwerp->looptijdeindeveiling.'</span> <button class="veilingDetail">Meer informatie</button> </div>
+                    </a>
+                </div>';
+        }
+    }
+    else {
+        echo 'Rubriek niet gevonden';
+    }
+}
+
+?>
