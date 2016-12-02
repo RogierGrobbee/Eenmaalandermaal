380d827b3c9cbbe7ffc2066e77bed94936f99153
Jamiel van Engen <luplet@hotmail.com>
2016-12-02 15:24:10 +0100

    Fixed index.php - Added the functionality to load the auctions which are about to close. - Cleaned the loadVeilingItems functionality

--- partial files/databaseconnection.php
+++ partial files/databaseconnection.php
@@ -32,41 +32,55 @@ function loadRubrieken(){
     return $rubriekArray;
 }
 
-function loadVeilingItems($rubriekId)
-{
+function loadVeilingItems($rubriekId) {
     if (is_numeric($rubriekId)) {
-        global $db;
-        $query = $db->query("select * from voorwerp where voorwerpnummer in(
+        displayVoorwerp("select * from voorwerp where voorwerpnummer in(
 	                          select voorwerpnummer from voorwerpinrubriek where rubriekoplaagsteniveau in
 	                          (
 		                        select rubrieknummer from rubriek where superrubriek='".$rubriekId."' or rubrieknummer = '".$rubriekId."'
 	                          )
                             )");
-        while ($voorwerp = $query->fetch(PDO::FETCH_OBJ)) {
-
-            $list = loadbestanden($voorwerp->voorwerpnummer);
-            $image = $list != null ? $list[0] : "NoImageAvalible.jpg";
-            echo '  <div class="veilingitem">
-                    <a href="./veiling.php?voorwerpnummer='.$voorwerp->voorwerpnummer.'">
-                        <img src="./bestanden/'.$image.'" alt="veilingsfoto">
-                        <h4>'. $voorwerp->titel .'</h4>
-                        <p>' . $voorwerp->beschrijving . '</p>
-                        <p class="prijs">€' . $voorwerp->startprijs . '</p>
-                        <div class="veiling-info">
-                            <div class="tijd">
-                                <span class="tijd-hidden">'.$voorwerp->looptijdeindeveiling.'</span>
-                                <span class="tijd-display"></span>
-                            </div>
-                            <button class="veiling-detail more-info">Meer informatie</button>
-                        </div>
-                    </a>
-                </div>';
-        }
     }
     else {
-        echo 'Rubriek niet gevonden';
+        displayVoorwerp("SELECT voorwerpnummer,
+                                    titel,
+                                    beschrijving,
+                                    looptijdeindeveiling,
+                                    startprijs
+                                    FROM voorwerp 
+                                      WHERE looptijdeindeveiling > DATEADD(MINUTE, 10, GETDATE())
+                                      ORDER BY looptijdeindeveiling ASC");
     }
 }
 
+function displayVoorwerp($queryString) {
+    global $db;
+
+    $query = $db->query($queryString);
+    while ($voorwerp = $query->fetch(PDO::FETCH_OBJ)) {
+
+        $list = loadbestanden($voorwerp->voorwerpnummer);
+        $image = $list != null ? $list[0] : "NoImageAvalible.jpg";
+        echoVoorwerp($voorwerp, $image);
+    }
+}
+
+function echoVoorwerp($voorwerp, $image) {
+    echo '<div class="veilingitem">
+                <a href="./veiling.php?voorwerpnummer='.$voorwerp->voorwerpnummer.'">
+                    <img src="./bestanden/'.$image.'" alt="veilingsfoto">
+                    <h4>'. $voorwerp->titel .'</h4>
+                    <p>' . $voorwerp->beschrijving . '</p>
+                    <p class="prijs">€' . $voorwerp->startprijs . '</p>
+                    <div class="veiling-info">
+                        <div class="tijd">
+                            <span class="tijd-hidden">'.$voorwerp->looptijdeindeveiling.'</span>
+                            <span class="tijd-display"></span>
+                        </div>
+                        <button class="veiling-detail more-info">Meer informatie</button>
+                    </div>
+                </a>
+          </div>';
+}
 
 ?>
