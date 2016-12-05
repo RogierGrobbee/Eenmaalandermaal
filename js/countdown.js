7dd86ff21bff337ff0656c8fc6f4a24e3ce596e4
Jamiel van Engen <luplet@hotmail.com>
2016-12-05 16:18:26 +0100

    countdown.js changes: - Changed countdown.js to make sure the date returned from the DB can be parsed to a Date. This change was implemented because a particular browser (looking at you IE) couldn't parse the date returned from the database. - Added comments to every Javascript function in the countdown.js.

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,25 @@
 /**
- * Created by jamiel on 30-11-2016.
+ * Created by Jamiel on 30-11-2016.
  */
 
+/**
+ * This function makes a new date for every browser (including IE) to parse to a Date.
+ * @param dateStr The date returned from mysql timestamp/datetime field.
+ * @returns {Date} The date that even IE can parse.
+ */
+function newDate(dateStr) {
+    var dateAndTimeSplit = dateStr.split(".")[0].split(" ");
+    var date = dateAndTimeSplit[0].split("-");
+    var time = dateAndTimeSplit[1].split(":");
+    return new Date(date[0],(date[1]-1),date[2],time[0],time[1],time[2]);
+}
+
+/**
+ * Formats a segment of the a timestamp (hours, minutes or seconds).
+ * If a segment is under 10 (Single digits) it will put a 0 at the beginning of the number.
+ * @param timeSegment The segment of time.
+ * @returns {*} A two digit segment.
+ */
 function formatTimeSegment(timeSegment) {
     if (timeSegment < 10) {
         timeSegment = '0' + timeSegment;
@@ -9,8 +27,14 @@ function formatTimeSegment(timeSegment) {
     return timeSegment;
 }
 
+/**
+ * Will display the remaining time between the current time and the time in the given time element.
+ * This function requires the time element to have a data-tijd attribute.
+ * @param timeElement The time element to display the remaining time.
+ */
 function showDifference(timeElement) {
-    var end = new Date(timeElement.getAttribute('data-tijd'));
+    var timeNoMilSec = timeElement.getAttribute('data-tijd').split('.')[0];
+    var end = newDate(timeNoMilSec);
 
     var _second = 1000;
     var _minute = _second * 60;
@@ -44,6 +68,9 @@ function showDifference(timeElement) {
     }
 }
 
+/**
+ * Function to refresh the remaining time of all the veiling items. Will get called every second.
+ */
 function refreshVeilingTijd () {
     var veilingsTijden = document.getElementsByClassName('tijd');
 
