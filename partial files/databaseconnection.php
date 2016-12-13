18c83b5afa0a3b61fd53a13e8d53c96b745f1786
Leon Chen <leonchen@live.nl>
2016-12-13 14:25:01 +0100

    veiling bied html css is bijna af

--- partial files/databaseconnection.php
+++ partial files/databaseconnection.php
@@ -260,6 +260,29 @@ function echoVoorwerp($voorwerp, $image)
                 </div>';
 }
 
+function echoHomepageVoorwerp($voorwerp, $image){
+    echo '<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 homepage-veiling">
+            <a href="veiling.php?voorwerpnummer='.$voorwerp->voorwerpnummer.'">
+            <img src="pics/'. $image .'"alt="veiling">
+            <h4>'.$voorwerp->titel.'</h4>
+            <div class="homepage-veiling-prijstijd">€'. $voorwerp->startprijs .'<br>
+            <span data-tijd="'. $voorwerp->looptijdeindeveiling .'" class="tijd"></span></div>
+            <button class="veiling-detail btn-homepage">Bied</button></a></div>';
+}
+
+function getVoorwerpBiedingen($voorwerpnummer){
+    global $db;
+
+    $query = $db->query("SELECT * FROM bod WHERE voorwerpnummer=$voorwerpnummer ORDER BY bodbedrag DESC");
+    $biedingen = array();
+
+    while ($bod = $query->fetch(PDO::FETCH_OBJ)) {
+        array_push($biedingen, $bod);
+    }
+
+    return $biedingen;
+}
+
 function strip_html_tags($str){
     $str = preg_replace('/(<|>)\1{2}/is', '', $str);
     $str = preg_replace(
@@ -288,16 +311,6 @@ function replaceWhitespace($str) {
         $result = str_replace($replacement, $replacement[0], $result);
     }
     return $str !== $result ? replaceWhitespace($result) : $result;
-}
-
-function echoHomepageVoorwerp($voorwerp, $image)
-{
-    echo '<div class="col-lg-4 col-md-6 col-sm-6 col-xs-11 homepage-veiling">
-                    <a href="veiling.php?voorwerpnummer=' . $voorwerp->voorwerpnummer . '">
-                    <img src="bestanden/' . $image . '"alt="veiling">
-                    <h4>' . $voorwerp->titel . '</h4>
-                    <div class="homepage-veiling-prijstijd">€' . $voorwerp->startprijs . '<br>
-                    <span data-tijd="' . $voorwerp->looptijdeindeveiling . '" class="tijd"></span></div>
 }
 
 function returnGeheimeVragen()
