6a3806b1013c47c6894386030f6b9e80fd9a5982
Robin <robinkro@hotmail.com>
2016-12-16 16:02:30 +0100

    Registratie

--- validatie.php
+++ validatie.php
@@ -13,7 +13,7 @@ if (isset($_POST['valideer'])) {
     $code = $_POST['validatiecode'];
     if (calculateExpire($code) == 1 && doesValidationCodeexist($code) == 1) {
         validateUser($code);
-        $errorString =  "Goedgekeurd, u kunt nu inloggen.";
+        $errorString =  "<div class='alert alert-success'>Goedgekeurd, u kunt nu inloggen.</div>";
 
     } else {
         $errorString =  "<div class='alert alert-danger'>Validatiecode niet correct of is verlopen.</div>";
