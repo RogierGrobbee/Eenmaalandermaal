2acde4a62be9875850bdc4effdf95e0e35baaa28
Jamiel van Engen <luplet@hotmail.com>
2016-12-19 15:01:25 +0100

    Additional changes - Added additional comments to the AJAXRequestHandler.php, endVeiling.php and countdown.js files. - Changed the word 'veiling' to auction.

---
+++ AJAXRequestHandler/AJAXRequestHandler.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamiel
+ * Date: 19-12-2016
+ * Time: 11:14
+ */
+
+/*
+ * This file is used to invoke the right function according to the AJAX.
+ * The AJAX NEEDS to send action to the server. The data in the Javascript needs this as a bare minimum:
+ * { action: 'someAction' }
+ * When a new action needs to be added it needs to be added to the switch statement in this file.
+ */
+
+include_once('endVeiling.php');
+
+/**
+ * Converts the output of 'file_get_contents('php://input')' to a hash table.
+ * @return array The php://input in a hash table.
+ */
+function fileContToHashTable() {
+    $content = file_get_contents('php://input');
+    $contentHashTable = array();
+
+    foreach (explode('&', $content) AS &$data) {
+        $dataSplit = explode('=', $data);
+        $key = $dataSplit[0];
+        $val = $dataSplit[1];
+        $contentHashTable[$key] = $val;
+    }
+
+    return $contentHashTable;
+}
+
+/**
+ * Checks if the posted data is legal (if the variable is not empty and set).
+ * @param $postData The variable to check .
+ * @return bool True if the given value is Legal, false if it's not.
+ */
+function isLegalPostData($postData) {
+    return isset ($postData) && !empty($postData);
+}
+
+$content = fileContToHashTable();
+
+$action = $content['action'];
+
+if (isLegalPostData($action)) {
+    switch ($action) {
+        case 'endAuction':
+            if (isLegalPostData($content['voorwerpnummer'])) {
+                endAuction($content['voorwerpnummer']);
+            }
+            break;
+    }
+}
---
+++ AJAXRequestHandler/endVeiling.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamiel
+ * Date: 16-12-2016
+ * Time: 10:16
+ */
+
+include_once('../partial files/databaseconnection.php');
+
+/**
+ * Ends the auction and emails the highest bidder and verkoper.
+ * @param $voorwerpId ID of the voorwerp.
+ */
+function endAuction($voorwerpId) {
+    if (!veilingEnded($voorwerpId)) {
+        $verkoper = getVerkoperByVoorwerpnummer($voorwerpId);
+        $highestBidder = getHighestBidderByVoorwerpnummer($voorwerpId);
+
+        if (mailAuctionEndedToGebruiker($verkoper, $voorwerpId)
+            && mailAuctionEndedToGebruiker($highestBidder, $voorwerpId)) {
+            endVeilingByVoorwerpnummer($voorwerpId);
+        }
+    }
+}
+
+function mailAuctionEndedToGebruiker($gebruiker, $voorwerpnummer) {
+    $email = $gebruiker->email;
+    $headers = 'From: webmaster@eenmaalandermaal.com' . "\r\n" .
+        'Reply-To: webmaster@eenmaalandermaal.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    return mail($email,
+        'EenmaalAndermaal: Veilingnummer: ' . $voorwerpnummer . ' afgelopen!',
+    "De veiling met het veilingnummer: " . $voorwerpnummer . ' is afgelopen!', $headers);
+}
+
+?>
