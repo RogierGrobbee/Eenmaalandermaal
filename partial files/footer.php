280cb8596aa9a384be8c484b4f2cfea1095d9b1f
Jamiel van Engen <luplet@hotmail.com>
2016-12-21 12:17:22 +0100

    Changes to the dbConnection - Removed the js from the databaseconnection.php - Added absolute path to the footer.php js paths - Added comments to the databaseconnection.php to indicate what the replacement will be

--- partial files/footer.php
+++ partial files/footer.php
@@ -10,7 +10,7 @@
             loadJSScripts();
         }
         ?>
-        <script type="text/javascript" src="js/filterSelect.js"></script>
-        <script type="text/javascript" src="js/navbar.js"></script>
+        <script type="text/javascript" src="http://iproject2.icasites.nl/js/filterSelect.js"></script>
+        <script type="text/javascript" src="http://iproject2.icasites.nl/js/navbar.js"></script>
     </body>
 </html>
