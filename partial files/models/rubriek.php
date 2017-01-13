6eb1788122da1f64ad81d9437578dd95a372432e
Leon Chen <leonchen@live.nl>
2017-01-13 14:26:26 +0100

    unspaghettify 3.0
    verder met feedback bezig

--- partial files/models/rubriek.php
+++ partial files/models/rubriek.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jamiel
- * Date: 20-12-2016
- * Time: 13:24
- */
+require_once ('databaseString.php');
 
 // Replaces: loadRubrieken()
 function loadAllRubrieken()
